33790aebd64006737bf53571d58ed85944e89053
mejsler <mejsler@pm.me>
2024-01-21 09:51:08 +0500

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const { stdin: input, stdout: output } = require('process');
+const rl = readline.createInterface({ input, output });
+const writeStream = fs.createWriteStream(`${path.resolve(__dirname, 'text.txt')}`);
+
+rl.on('close', () => console.log('Mission accomplished'));
+rl.setPrompt(`Waiting for input (type 'exit' to close the program):\n`);
+rl.prompt();
+rl.on('line', (answer) => {
+  if (answer === 'exit') {
+    rl.close();
+  } else {
+    writeStream.write(`${answer}\n`);
+  }
+});
